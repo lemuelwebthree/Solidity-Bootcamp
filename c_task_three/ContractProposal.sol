decfa86d7055e7246f1f8de009ccba87bfbadb13
lemusosa <alvinandwealth@gmail.com>
2023-11-21 14:32:46 +0200

    Counter.sol util library contracted added in

--- c_task_three/ContractProposal.sol
+++ c_task_three/ContractProposal.sol
@@ -19,7 +19,7 @@ contract ContractProposal {
             uint256 approve; // Number of approve votes
             uint256 reject; // Number of reject votes
             uint256 pass; // NUmber of pass votes
-            uint256 total_vote_end; // When the total votes in the proposal reches this limit, proposal ends
+            uint256 total_vote_to_end; // When the total votes in the proposal reches this limit, proposal ends
             bool current_state; // This shows the current state of the propsoal, meaning whether if passes of failes
             bool is_active; // This shows if others can vote to our contract
     }
